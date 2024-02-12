780ab980c4acf5448d7bb6bb7cf5d787075c0654
buhp <buhp@buhp.ch>
2024-02-12 19:52:21 +0100

    gitignore hinzugefügt

--- DUAL_PORT_RAM.v
+++ DUAL_PORT_RAM.v
@@ -1,8 +1,8 @@
 module DUAL_PORT_RAM (
-    input clk_a, clk_b, enable_a, write_a, enable_b;
-    input [15:0] address_a, address_b;
-    input [7:0] data_in_a;
-    output [7:0] data_out_b;
+    input clk_a, clk_b, enable_a, write_a, enable_b,
+    input [15:0] address_a, address_b,
+    input [7:0] data_in_a,
+    output [7:0] data_out_b
 );
 
     reg [15:0] ram [1023:0];
@@ -23,7 +23,7 @@ module DUAL_PORT_RAM (
     begin
         if (enable_b)
         begin
-            data_out <= ram[address_b];
+            data_b <= ram[address_b];
         end
     end
 
